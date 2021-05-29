ea07ce53ebab446e3a94b53100c52e8e57d07514
c-gayan <85017193+c-gayan@users.noreply.github.com>
2021-05-29 16:21:54 +0530

    t1

---
+++ src/commands/test/TestembedCommand.js
@@ -0,0 +1,22 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+module.exports = class TestembedCommand extends BaseCommand {
+  constructor() {
+    super('testembed', 'test', []);
+  }
+
+  async run(client, message, args) {
+    console.log('repling');
+    const em = new Discord.MessageEmbed()
+        .setColor('#04ff00')
+        .setTitle('title 1')
+        .setDescription('this is discription of the msg.')
+        .setAuthor(message.author.username)
+        .addField('this embed works', 'field description')
+        .setThumbnail('https://i.imgur.com/PyVmvKL.jpg')
+        .setFooter('footer of the msg')
+        .setTimestamp()
+        .setURL('https://google.com')
+        message.channel.send(em);
+  }
+}
